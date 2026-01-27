11a99de8fbdb847944a3c97e32e6a9e8132c4647
Daniel Sângelo <danielsangelo@hotmail.com>
2026-01-27 18:37:46 +0000

    feat: create navbar component

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -3,16 +3,30 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
         <title>{{ $title ?? 'Page Title' }}</title>
-
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
-
         @vite(['resources/css/app.css', 'resources/js/app,js'])
     </head>
-    <body class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
-        {{ $slot }}
+    <body class="bg-[#FDFDFC] text-[#1b1b18]">
+        @auth
+            <x-ui.nav>
+                <x-ui.nav.item route="home">
+                    Home
+                </x-ui.nav.item>
+                <x-ui.nav.item route="admin.raffle">
+                    Raffle
+                </x-ui.nav.item>
+                {{-- <x-ui.nav.item route="logout">
+                    Logout
+                </x-ui.nav.item> --}}
+            </x-ui.nav>
+        @endauth
+        <x-ui.container>
+
+            {{ $slot }}
+
+        </x-ui.container>
     </body>
 </html>
