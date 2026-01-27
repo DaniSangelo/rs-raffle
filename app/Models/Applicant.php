4744b30948323947010281d70dddf23bea52cafb
Daniel Sângelo <danielsangelo@hotmail.com>
2026-01-27 15:38:42 +0000

    feat: create migrations, models, factories and seeders

---
+++ app/Models/Applicant.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Applicant extends Model
+{
+    /** @use HasFactory<\Database\Factories\ApplicantFactory> */
+    use HasFactory;
+
+    public function raffle()
+    {
+        return $this->belongsTo(Raffle::class);
+    }
+}
---
+++ app/Models/Raffle.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Raffle extends Model
+{
+    /** @use HasFactory<\Database\Factories\RaffleFactory> */
+    use HasFactory;
+
+    public function applicants()
+    {
+        return $this->hasMany(Applicant::class);
+    }
+}
