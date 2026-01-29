0f51d0fcc924d1b943d0735b97333d0048bd8f02
Daniel Sângelo <danielsangelo@hotmail.com>
2026-01-29 11:37:59 +0000

    feat: create error blade component to show errors

---
+++ resources/views/components/ui/error.blade.php
@@ -0,0 +1,8 @@
+@props([
+    'name',
+])
+@error($name)
+    <div class="text-red-500 text-sm mt-2">
+        {{ $message }}
+    </div>
+@enderror
--- resources/views/components/ui/input.blade.php
+++ resources/views/components/ui/input.blade.php
@@ -3,9 +3,5 @@
     <input {{ $attributes }}
         class="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 dark:border-gray-700">
 
-    @error($attributes->get('name'))
-        <div class="text-red-500 text-sm mt-2">
-            {{ $message }}
-        </div>
-    @enderror
+    <x-ui.error name="{{ $attributes->get('name') }}" />
 </div>
